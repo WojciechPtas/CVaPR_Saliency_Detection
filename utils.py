e68b285b05b6cdb403f9b30e13e37a1ac7f1ffb4
WojciechPtas <wojciech.ptas@gmail.com>
2024-05-30 21:52:09 +0200

    Move basic utilities to utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+import cv2
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file = sys.stderr, **kwargs)
+
+def load_image(path):
+    img = cv2.imread(str(path))
+    (h, w, c) = img.shape[:3]
+    return (img, h, w, c)
